36ece39deb9e176fad2de3b484513dce18894acd
Popescu Dan <popedan99@yahoo.com>
2024-09-19 01:57:04 +0300

    Updated code to work with change color to result + typo issue

--- index.js
+++ index.js
@@ -14,48 +14,31 @@ function playGame(playerChoice) {
   let result = "";
 
   if (playerChoice === computerChoice) {
-    result = "It's a Tie !";
+    result = "It's a Tie!";
   } else {
     switch (playerChoice) {
       case "rock":
-        result =
-          computerChoice === "scissors"
-            ? "You Win!"
-            : computerChoice === "rock"
-            ? "It's a Tie!"
-            : "You Lose!";
+        result = computerChoice === "scissors" ? "You Win!" : "You Lose!";
         break;
       case "paper":
-        result =
-          computerChoice === "rock"
-            ? "You Win!"
-            : computerChoice === "paper"
-            ? "It's a Tie!"
-            : "You Lose!";
+        result = computerChoice === "rock" ? "You Win!" : "You Lose!";
         break;
       case "scissors":
-        result =
-          computerChoice === "paper"
-            ? "You Win!"
-            : computerChoice === "scissors"
-            ? "It's a Tie!"
-            : "You Lose!";
+        result = computerChoice === "paper" ? "You Win!" : "You Lose!";
         break;
     }
   }
   playerDisplay.textContent = `PLAYER: ${playerChoice}`;
   computerDisplay.textContent = `Computer: ${computerChoice}`;
   resultDisplay.textContent = result;
-
   resultDisplay.classList.remove("greenText", "redText");
-
   switch (result) {
-    case "You Win !":
+    case "You Win!":
       resultDisplay.classList.add("greenText");
       playerScore++;
       playerScoreDisplay.textContent = playerScore;
       break;
-    case "You Lose !":
+    case "You Lose!":
       resultDisplay.classList.add("redText");
       computerScore++;
       computerScoreDisplay.textContent = computerScore;
